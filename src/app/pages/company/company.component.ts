4c4b8d73e9dae8b196f0e4076926137413d9b632
Gago <egagik69@gmail.com>
2025-03-06 21:26:44 +0400

    start Zfloor project

---
+++ src/app/pages/company/company.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zfloor-company',
+  imports: [],
+  templateUrl: './company.component.html',
+  styleUrl: './company.component.scss'
+})
+export class CompanyComponent {
+
+}
---
+++ src/app/pages/stocks/stocks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zfloor-stocks',
+  imports: [],
+  templateUrl: './stocks.component.html',
+  styleUrl: './stocks.component.scss'
+})
+export class StocksComponent {
+
+}
